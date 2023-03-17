03bc33f8251a3a9083a1f35828b55b9e9c45e2e7
Kavya Naveena <karumurikavya@gmail.com>
2023-03-17 18:59:44 +0530

    update

--- aws-parameters/state.tf
+++ aws-parameters/state.tf
@@ -5,7 +5,6 @@ terraform{
 resource "aws_ssm_parameter" "parameters" {
   count=length(var.parameters)
   name  = "var.parameters[count.index].name"
-  type  = "var.parameters[count.index].type"
   value = "var.parameters[count.index].value"
 }
 
