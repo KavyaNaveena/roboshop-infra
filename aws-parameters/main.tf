2066a30e0a07b1b686a0ea866d165209c5cbb39c
Kavya Naveena <karumurikavya@gmail.com>
2023-03-18 13:11:24 +0530

    update

---
+++ aws-parameters/main.tf
@@ -0,0 +1,9 @@
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = var.parameters[count.index].type
+}
+
+variable "parameters" {
+}
