fefef02f14199f4bc3625d8981f3003ffb2c6e31
Kavya Naveena <karumurikavya@gmail.com>
2023-03-20 13:26:51 +0530

    update

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
-  iam_instance_profile = "${var.component}-${var.env}-role"
+  iam_instance_profile = "${var.env}-${var.component}-role"
   tags                   = {
     Name = var.component
   }
@@ -16,7 +16,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-        "ansible-pull -i localhost, -U https://github.com/KavyaNaveena/roboshop-ansible.git roboshop.yml -e role_name=${var.component} -e env=${var.env}"
+        "ansible-pull -i localhost, -U https://github.com/KavyaNaveena/roboshop-ansible.git roboshop.yml -e role_name=${var.component}"
      ]
   }
 }
@@ -41,7 +41,7 @@ resource "aws_security_group" "sg" {
   }
 
   tags = {
-    Name = "${var.component}-${var.env}-sg"
+    Name = "${var.env}-${var.component}-sg"
   }
 }
 
